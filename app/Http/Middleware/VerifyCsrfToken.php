61d2570af45da8492b478bbb426daec612fb1090
Anne Jan Brouwer <brouwer@annejan.com>
2020-11-24 23:16:23 +0100

    Ignore CSRF in testing and local

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,9 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Support\Facades\App;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -14,4 +17,25 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request  $request
+     * @param \Closure $next
+     *
+     * @throws TokenMismatchException
+     *
+     * @return mixed
+     */
+    public function handle($request, \Closure $next)
+    {
+        // Don't validate CSRF when testing.
+        if (App::environment(['local', 'testing'])) {
+            return $this->addCookieToResponse($request, $next($request));
+        }
+        // @codeCoverageIgnoreStart
+        return parent::handle($request, $next);
+        // @codeCoverageIgnoreEnd
+    }
 }
